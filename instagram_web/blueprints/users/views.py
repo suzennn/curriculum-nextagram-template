cfb87771ef8ea21ccf0f3b07c02cff70b527ba71
suzennn <52767681+suzennn@users.noreply.github.com>
2019-08-27 11:03:03 +0800

    added functional sign up feature; midway through functional login feature

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,20 +1,48 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect, flash
+from models import *
+from werkzeug.security import check_password_hash
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
 
 
-@users_blueprint.route('/new', methods=['GET'])
+@users_blueprint.route('/new')
 def new():
     return render_template('users/new.html')
 
-
-@users_blueprint.route('/', methods=['POST'])
+@users_blueprint.route('/new', methods=['POST'])
 def create():
-    pass
+    if request.form["password"] != request.form["confirmpwd"]:
+        flash('passwords do not match','error')
+        return render_template("users/new.html")
+    else:
+        new_user = user.User(name=request.form["name"],username=request.form["username"],email=request.form["email"],password=request.form["password"])
+        if new_user.save():
+            flash("Successfully saved!",'success')
+            return redirect("/")
+        else:
+            flash('<br>'.join(new_user.errors),'error')
+            return render_template("users/new.html",errors=new_user.errors)
 
+@users_blueprint.route('/login')
+def login():
+    return render_template('users/login.html')
+
+# @users_blueprint.route('/login', method=["POST"])
+def login_user():
+    try:
+        user.User.get(user.User.username == request.form['userlogin'])
+        user_check = user.User.get(user.User.username == request.form['userlogin'])
+        pwd_check = request.form['pwdlogin']
+        result = check_password_hash(user_check.password,pwd_check)
+        if result:
+            flash("Login successful! Welcome back~","success")
+        else:
+            flash("password is incorrect. please try again","error")
+    except:
+        flash("user does not exist","error")
+        return render_template("users/login.html")
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
@@ -34,3 +62,5 @@ def edit(id):
 @users_blueprint.route('/<id>', methods=['POST'])
 def update(id):
     pass
+
+
--- models/user.py
+++ models/user.py
@@ -1,6 +1,28 @@
 from models.base_model import BaseModel
+from werkzeug.security import generate_password_hash
 import peewee as pw
-
+import re
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    name = pw.CharField(null=False)
+    email = pw.CharField(unique=True,null=False)
+    username = pw.CharField(unique=True,null=False)
+    password = pw.CharField(null=False)
+
+
+    def validate(self):
+        pattern_pwd = "(?=.*[A-Z])(?=.*[!@#$&*\^%\*\.])(?=.*[0-9])(?=.*[a-z]).{6,}$"
+        check_pwd = re.match(pattern_pwd,self.password)     #match & findall works the same
+
+        if not len(self.name):
+            self.errors.append("name field is empty")
+        if not len(self.username):
+            self.errors.append("username field is empty")
+        if not len(self.email):
+            self.errors.append("email field is empty")
+        if not len(self.password):
+            self.errors.append("password field is empty")
+        if not check_pwd:
+            self.errors.append("password does not meet minimum requirement")
+        else:
+            self.password = generate_password_hash(self.password)
